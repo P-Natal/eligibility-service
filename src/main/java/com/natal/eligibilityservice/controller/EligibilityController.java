20f3ab604d53fae5c489d1c2c03b165a80a7c1f1
Pedro Natal <pedro.natal@m4u.com.br>
2022-12-28 20:58:25 -0300

    Fixing response types

--- src/main/java/com/natal/eligibilityservice/controller/EligibilityController.java
+++ src/main/java/com/natal/eligibilityservice/controller/EligibilityController.java
@@ -3,6 +3,8 @@ package com.natal.eligibilityservice.controller;
 import com.natal.eligibilityservice.service.EligibilityService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,15 +18,18 @@ public class EligibilityController {
     private EligibilityService eligibilityService;
 
     @GetMapping("/{document}")
-    public EligibilityResponseTO getEligibility(@PathVariable("document") String document){
+    public ResponseEntity<EligibilityResponseTO> getEligibility(@PathVariable("document") String document){
+        if (!CpfValidator.isValid(document)){
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
         log.info("Recebendo requisicao para elegibilidade do cliente com documento {}", document);
-        return eligibilityService.getEligibility(document);
+        return new ResponseEntity<>(eligibilityService.getEligibility(document), HttpStatus.OK);
     }
 
     @PostMapping
-    public EligibilityResponseTO setEligibility(@Valid @RequestBody ClientEligibityTO clientEligibityTO){
+    public ResponseEntity<EligibilityResponseTO> setEligibility(@Valid @RequestBody ClientEligibityTO clientEligibityTO){
         log.info("Recebendo requisicao para criacao de elegibilidade com body {}", clientEligibityTO);
-        return eligibilityService.create(clientEligibityTO);
+        return new ResponseEntity<>(eligibilityService.create(clientEligibityTO), HttpStatus.OK);
     }
 
 }
